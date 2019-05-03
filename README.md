faf11946ebe255250108ad2a6ee4ef2c78eb741d
Balázs Indig <indba@digitus.itk.ppke.hu>
2019-05-03 22:23:11 +0200

    Refactor converter for integration

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ emCoNLL is a (half-baked) script converting [emtsv](https://github.com/dlt-rilmt
 
 What's in this repo:
 
-* [main.py](main.py) the script itself
+* [converter.py](converter.py) the script itself
 * [vizilo.tsv](vizilo.tsv) small input example
 * [vizilo.conll](vizilo.conll) small output example
 * [conll_features](conll_features) about the formats
 
 Usage:
-`python3 main.py vizilo.tsv`
+`cat vizilo.tsv python3 converter.py > vizilo.tsv`
 
---
+++ converter.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8, vim: expandtab:ts=4 -*-
+
+import sys
+
+
+class EmCoNLL:
+
+    def __init__(self, source_fields=None, target_fields=None):
+        self._col_mapper = {'id': 'ID',
+                            'form': 'FORM',
+                            'lemma': 'LEMMA',
+                            'upostag': 'UPOS',
+                            'xpostag': 'XPOS',
+                            'feats': 'FEATS',
+                            'head': 'HEAD',
+                            'deprel': 'DEPREL'
+                            }
+
+        # The CoNLL columns in order
+        self._conll = ['ID', 'FORM', 'LEMMA', 'UPOS', 'XPOS', 'FEATS', 'HEAD', 'DEPREL', 'DEPS', 'MISC']
+
+        # Field names for e-magyar TSV
+        if source_fields is None:
+            source_fields = set()
+
+        if target_fields is None:
+            target_fields = []
+
+        self.source_fields = source_fields
+        self.target_fields = target_fields
+
+    def process_sentence(self, sen, field_names):
+        """
+        Reorder the needed fields and put _ when a mandatory field missing (eg. not created yet)
+        :param sen: The sentence splitted to tokens and fields
+        :param field_names: The name of the fields mapped to the column indices
+        :return: A generator yields the output line-by-line
+        """
+
+        for line in sen:
+            new_line = (line[field_names[col]] if col in field_names else '_'
+                        for col in self._conll)
+            yield new_line
+
+    def prepare_fields(self, field_names):
+        """
+        Map the mandatory emtsv field names to the CoNLL names tied to the current indices
+        :param field_names: emtsv header
+        :return: Mapping of the mandatory CoNLL field names to the current indices
+        """
+
+        return {self._col_mapper[emtsv_name]: col_num for col_num, emtsv_name in field_names.items()
+                if emtsv_name in self._col_mapper}
+
+
+def main():
+    conv = EmCoNLL()
+    input_iterator = sys.stdin
+    output_iterator = sys.stdout
+    # Header with the field names
+    header = {i: name for i, name in enumerate(input_iterator.readline().strip().split())}
+    # Map the required field names to the indices
+    field_names = conv.prepare_fields(header)
+    # Convert sentence-by-sentence
+    curr_sen = []
+    for line in input_iterator:
+        line = line.strip()
+        if len(line) > 0:
+            curr_sen.append(line.split('\t'))
+        else:
+            if curr_sen:  # End of sentence
+                output_iterator.writelines('{0}\n'.format('\t'.join(line))
+                                           for line in conv.process_sentence(curr_sen, field_names))
+                output_iterator.write('\n')
+                curr_sen = []
+
+
+if __name__ == '__main__':
+    main()
