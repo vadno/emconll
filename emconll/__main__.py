b47017eeb3c77bf10bd683cf1643c5e8c0433f50
Balázs Indig <indba@digitus.itk.ppke.hu>
2020-09-03 21:02:32 +0200

    Add new options to support multiple standardish formats
    - Print header
    - Force ID genration
    - Add SpaceAfter=no to misc column when wsafter field present
    - Add extra columns to include extra fields in the output

--- emconll/__main__.py
+++ emconll/__main__.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from xtsv import build_pipeline, parser_skeleton
+from xtsv import build_pipeline, parser_skeleton, add_bool_arg
 
 
 def main():
 
     argparser = parser_skeleton(description='emCoNLL - a script converting emtsv output to CoNLL-U format')
+    add_bool_arg(argparser, 'print-header', 'Print header')
+    add_bool_arg(argparser, 'force-id', 'Force writing ID field when it is not available')
+    add_bool_arg(argparser, 'add-space-after-no', 'Add SpaceAfter=no to misc when wsafter field present')
+    argparser.add_argument('--extra-columns', dest='extra_columns',  type=str, default=None,
+                           help='Add extra columns in key1:val1,key2:val2 format')
     opts = argparser.parse_args()
+    extra_columns_str = opts.extra_columns
+    extra_columns = {}
+    if extra_columns_str is not None:
+        kws = extra_columns_str.split(',')
+        for kw in kws:
+            k, v = kw.split(':', maxsplit=1)
+            extra_columns[k] = v
 
     # Set input and output iterators...
     if opts.input_text is not None:
@@ -24,7 +36,8 @@ def main():
 
     # The relevant part of config.py
     # from emdummy import DummyTagger
-    em_conll = ('emconll.converter', 'EmCoNLL', 'CoNLL-U converter', (),
+    em_conll = ('emconll.converter', 'EmCoNLL', 'CoNLL-U converter',
+                (opts.print_header, opts.force_id, opts.add_space_after_no, extra_columns),
                 {'source_fields': {'form'}, 'target_fields': []})
     tools = [(em_conll, ('conll', 'emCoNLL'))]
 
--- emconll/converter.py
+++ emconll/converter.py
@@ -5,7 +5,8 @@
 class EmCoNLL:
     pass_header = False
 
-    def __init__(self, source_fields=None, target_fields=None):
+    def __init__(self, print_header=False, force_id=False, add_space_after_no=False, extra_columns=None,
+                 source_fields=None, target_fields=None):
         # From emtsv format to CoNLL-U (in CoNLL-U order)
         self._col_mapper = {'id': 'ID',
                             'form': 'FORM',
@@ -19,6 +20,23 @@ class EmCoNLL:
                             'misc': 'MISC'
                             }
 
+        if extra_columns is not None:
+            if len(self._col_mapper.keys() & extra_columns.keys()) > 0 or \
+                    len(set(self._col_mapper.values()) & set(extra_columns.values())) > 0 or \
+                    len(set(self._col_mapper.values())) < len(self._col_mapper.values()) or \
+                    len(self._col_mapper.keys()) + len(extra_columns.keys()) != \
+                    len(self._col_mapper.keys() | extra_columns.keys()):
+                raise ValueError(f'Some extra_column input or output name is duplicated:'
+                                 f' {self._col_mapper} and {extra_columns} !')
+            self._col_mapper.update(extra_columns)
+        self._print_header = print_header
+        self._force_id = force_id
+        self._add_id = self._dummy_fun
+        self._add_space_after_no = add_space_after_no
+        self._add_space_after_no_fun = self._dummy_fun
+        self._ws_after_column_no = -1
+        self._sentence_count = 0
+
         # Field names for e-magyar TSV
         if source_fields is None:
             source_fields = set()
@@ -30,16 +48,38 @@ class EmCoNLL:
         self.target_fields = target_fields
 
     @staticmethod
-    def process_sentence(sen, field_names):
+    def _dummy_fun(_, new_line):
+        return new_line
+
+    def _add_space_after_no_real_fun(self, orig_line, new_line):
+        if orig_line[self._ws_after_column_no] == '""':
+            misc = 'SpaceAfter=No'
+        else:
+            misc = '_'
+        new_line[9] = misc  # wsafter -> misc
+        return new_line
+
+    @staticmethod
+    def _add_id_fun(i, new_line):
+        new_line[0] = str(i)
+        return new_line
+
+    def process_sentence(self, sen, field_names):
         """
         Reorder the needed fields and put _ when a mandatory field missing (eg. not created yet)
         :param sen: The sentence splitted to tokens and fields
         :param field_names: The name of the fields mapped to the column indices
         :return: A generator yields the output line-by-line
         """
+        self._sentence_count += 1
 
-        for line in sen:
-            new_line = (line[col] if col != '_' else '_' for col in field_names)
+        if self._print_header and self._sentence_count == 1:
+            yield self._col_mapper.values()
+
+        for i, line in enumerate(sen, start=1):
+            new_line = [line[col] if col != '_' else '_' for col in field_names]
+            new_line = self._add_id(i, new_line)
+            new_line = self._add_space_after_no_fun(line, new_line)
             yield new_line
 
     def prepare_fields(self, field_names):
@@ -49,4 +89,11 @@ class EmCoNLL:
         :return: (list) The "mapping" of the mandatory CoNLL field names to the current indices
         """
 
-        return [field_names.get(emtsv_name, '_') for emtsv_name in self._col_mapper.keys()]
+        fields_nums = [field_names.get(emtsv_name, '_') for emtsv_name in self._col_mapper.keys()]
+        if self._add_space_after_no and fields_nums[9] == '_' and field_names.get('wsafter') is not None:
+            self._add_space_after_no_fun = self._add_space_after_no_real_fun
+            self._ws_after_column_no = field_names['wsafter']
+        if self._force_id and fields_nums[0] == '_':
+            self._add_id = self._add_id_fun
+
+        return fields_nums
